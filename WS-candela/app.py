16d2d56fe88ad6dba4d2f96b49fa0e39919669d9
Javi de Alcazar <javierdealcazar98@gmail.com>
2024-01-13 12:59:46 +0100

    add try except in endpoint

--- WS-candela/app.py
+++ WS-candela/app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify, redirect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from cerberus import Validator
+import threading
+from queue import Queue
 from werkzeug.serving import make_server
 import signal
 
@@ -17,6 +19,8 @@ my_module_path = os.path.join(script_dir, "..")
 sys.path.append(my_module_path)
 os.chdir(os.path.dirname(__file__))
 
+queue_info = Queue()
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 limiter = Limiter(
@@ -28,7 +32,6 @@ limiter = Limiter(
 def home():
    return "API"
 
-
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
     if request.method == 'POST':
@@ -37,14 +40,15 @@ def shutdown():
         return jsonify(message="Server shutting down..."), 200
     else:
         return jsonify(error="Invalid request method"), 405
-    
+
+def ws_candela(cups):
+        info = ws_app.webscraping_chrome_candelas(cups)
+        queue_info.put(info)
 
 @app.route('/cups20', methods=['GET'])
 @limiter.limit("10 per minute")
 def calcule_energy_consumption():
     
-    # response = requests.get('https://example.com', verify=True)
-
     schema = {
     'cups20': {'type': 'string', 'minlength': 20, 'maxlength': 22},
     }
@@ -52,14 +56,20 @@ def calcule_energy_consumption():
 
     record = json.loads(request.data)
 
-    if validator.validate(record):
-        cups20 = record["cups20"]
-        info = ws_app.webscraping_chrome_candelas(cups20)
-        return {"info": info}
-    else:
-        return {'error': f'Data is invalid {validator.errors}'}
-
-
+    try:
+        if validator.validate(record):
+            cups = record["cups20"]
+            print(cups)
+            thread = threading.Thread(target=ws_candela, args=(cups,))
+            thread.start()
+            thread.join()
+            info = queue_info.get()
+            return {"info": info}
+        else:
+            return {'error': f'Data is invalid {validator.errors}'}
+    except Exception as e:
+        return {'error': e}
+    
 if __name__ == '__main__':
-  server = make_server('0.0.0.0', 5000, app)
+  server = make_server('127.0.0.1', 5000, app)
   server.serve_forever()
