073c672c5b7473625adbe3fafa9f028156fd88f4
Juan Antonio Devesa Thomson <juanadevesat93@gmail.com>
2024-01-13 23:37:08 +0100

    aaaaaaaaaaaaaa

--- server/config/sqlConnection.js
+++ server/config/sqlConnection.js
@@ -4,11 +4,6 @@ require('dotenv').config();
 const db = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, `${process.env.SQL_PWD}`, {
     host: process.env.SQL_HOST,
     dialect: 'postgres',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: process.env.CA_CERT,
-    },
     define: {
         freezeTableName: true,
         timestamps: false,
