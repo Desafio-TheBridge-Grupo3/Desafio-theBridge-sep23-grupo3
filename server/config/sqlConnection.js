68418d658653d32c9c069abd3bff9ce1440fdb56
Juan Antonio Devesa Thomson <juanadevesat93@gmail.com>
2024-01-13 14:06:55 +0100

    ligera modificacion a server

--- server/config/sqlConnection.js
+++ server/config/sqlConnection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, `${process.env.SQL_PWD}`, {
+const db = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PWD, {
     host: process.env.SQL_HOST,
     dialect: 'postgres',
     define: {
